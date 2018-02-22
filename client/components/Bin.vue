7c8e9be9959803e0f32885ee8859f9a9a4049ce2
Hayden Bickerton <haydenbbickerton@gmail.com>
2018-02-21 21:38:51 -0600

    Add bin component

---
+++ client/components/Bin.vue
@@ -0,0 +1,71 @@
+<style scoped>
+.bin {
+  display: grid;
+  width: 150px;
+  height: 150px;
+  text-align: center;
+  line-height: 48px;
+  font-size: 1rem;
+  color: white;
+  background-color: black;
+  z-index: 50;
+  margin: auto;
+
+  &.bin--blue {
+    background-color: blue;
+  }
+  &.bin--green {
+    background-color: green;
+  }
+  &.bin--red {
+    background-color: red;
+  }
+  .disarmed-count {
+    margin: auto;
+  }
+}
+</style>
+
+<template>
+  <div class="bin-wrapper">
+    <div class="bin" v-bind:class="[binColorClass]" v-bind:style="{transform: binSize}">
+      <div class="disarmed-count">
+        {{disarmedCount}}
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  props: ['bin'],
+  computed: {
+    binColorClass () {
+      return `bin--${this.bin.color}`
+    },
+    binSize () {
+      const increase = this.disarmedCount * 0.01
+      return `scale(${1 + increase})`
+    },
+    disarmedCount () {
+      return this.disarmedBombs.filter(bomb => {
+        return bomb.color === this.bin.color
+      }).length
+    },
+    ...mapGetters([
+      'disarmedBombs'
+    ])
+  },
+  updated() {
+    this.reCalculatePosition()
+  },
+  methods: {
+    reCalculatePosition () {
+      this.bin.rect = this.$el.getBoundingClientRect();
+    }
+  }
+}
+</script>
